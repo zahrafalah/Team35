059bc389ffcd3abe84b5563f6e6a415e1fa5c396
Sepideh <sepidehfallah2@gmail.com>
2021-11-07 20:41:45 -0700

    adding .gitignore

--- dbData/patients.sql
+++ dbData/patients.sql
@@ -7,11 +7,38 @@ create table patients(
    PRIMARY KEY ( _id )
 );
 
+create table nurses(
+   _id INT NOT NULL AUTO_INCREMENT,
+   username VARCHAR(100) NOT NULL UNIQUE,
+   password VARCHAR(40) NOT NULL,
+   PRIMARY KEY ( _id )
+);
+create table doctors(
+   _id INT NOT NULL AUTO_INCREMENT,
+   username VARCHAR(100) NOT NULL UNIQUE,
+   password VARCHAR(40) NOT NULL,
+   PRIMARY KEY ( _id )
+);
+
 INSERT INTO patients(username,password) VALUES ('Zahra', '123456');
-INSERT INTO patients(username,password) VALUES ('Yogesh', '123456');
+INSERT INTO patients(username,password) VALUES ('Yogesh', '123456');  
 INSERT INTO patients(username,password) VALUES ('Nipoon', '123456');
 INSERT INTO patients(username,password) VALUES ('Kartavya', '123456');
 INSERT INTO patients(username,password) VALUES ('Uday', '123456');
 
+INSERT INTO nurses(username,password) VALUES ('Zahra', '123456');
+INSERT INTO nurses(username,password) VALUES ('Yogesh', '123456');  
+INSERT INTO nurses(username,password) VALUES ('Nipoon', '123456');
+INSERT INTO nurses(username,password) VALUES ('Kartavya', '123456');
+INSERT INTO nurses(username,password) VALUES ('Uday', '123456');
+
+INSERT INTO doctors(username,password) VALUES ('Zahra', '123456');
+INSERT INTO doctors(username,password) VALUES ('Yogesh', '123456');  
+INSERT INTO doctors(username,password) VALUES ('Nipoon', '123456');
+INSERT INTO doctors(username,password) VALUES ('Kartavya', '123456');
+INSERT INTO doctors(username,password) VALUES ('Uday', '123456');
+
 SELECT * FROM patients;
-DROP TABLE patients;
+DROP TABLE Doctors ;
+
+SELECT * FROM  patients WHERE username = 'Zahra' AND password = '123456';
