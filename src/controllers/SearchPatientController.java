859f81d63f8fd715557bc28abec2a83fa1a86855
uday-asu <upolishe@asu.edu>
2021-11-10 12:37:48 -0700

    corrected wrong code

--- src/controllers/SearchPatientController.java
+++ src/controllers/SearchPatientController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuButton;
