5acc456bf15fffd8cb325964f62ec65f9690fe59
Sepideh <sepidehfallah2@gmail.com>
2021-11-11 16:34:02 -0700

    Logout function has been refactored to a new class

--- src/controllers/SearchPatientController.java
+++ src/controllers/SearchPatientController.java
@@ -44,18 +44,8 @@ public class SearchPatientController {
 	private Doctor doctor;
 	
 	public void Logout(ActionEvent event) {
-		try {
-			System.out.println("attempting logout");
-			Stage stage = (Stage) ((Node)event.getTarget()).getScene().getWindow();	
-			
-			Parent root =  FXMLLoader.load(getClass().getResource("/pages/Login.fxml"));
-			stage.setScene(new Scene(root));
-			stage.show();
-			
-		}catch(Exception ex) {
-			System.err.println(ex.getMessage());
-				
-		}
+		LogoutController logout = new LogoutController();
+		logout.Logout(event);
 	}
 	
 	public void SetPatient(Patient patient) {
